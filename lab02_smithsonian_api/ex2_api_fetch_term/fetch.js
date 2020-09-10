382a08bea389f1ef225a52378c7f6fb8d499f9b4
Sohee Cho <soheecho94@gmail.com>
2020-09-10 15:15:05 -0400

    remove api key

--- lab02_smithsonian_api/ex2_api_fetch_term/fetch.js
+++ lab02_smithsonian_api/ex2_api_fetch_term/fetch.js
@@ -2,7 +2,7 @@
 // check API documentation for terms here: http://edan.si.edu/openaccess/apidocs/#api-search-terms
 
 // put your API key here;
-const apiKey = "qEIgONMDA9l4wPFA864Vvldpm2tfKzD2LJtAELFK";  
+const apiKey = "";  
 
 // Access to terms by term category (I.e. online_media_type > Images)
 const termBaseURL = "https://api.si.edu/openaccess/api/v1.0/terms/";
