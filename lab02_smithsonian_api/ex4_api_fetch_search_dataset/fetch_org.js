c956efee0798114b84b064b7f98a3da21684a49f
readyletsgo <rich@rt80.net>
2020-09-29 16:16:55 -0400

    example for James

---
+++ lab02_smithsonian_api/ex4_api_fetch_search_dataset/fetch_org.js
@@ -0,0 +1,128 @@
+// Smithsonian API example code
+// check API documentation for search here: http://edan.si.edu/openaccess/apidocs/#api-search-search
+// Using this data set https://collections.si.edu/search/results.htm?q=Flowers&view=grid&fq=data_source%3A%22Cooper+Hewitt%2C+Smithsonian+Design+Museum%22&fq=online_media_type%3A%22Images%22&media.CC0=true&fq=object_type:%22Embroidery+%28visual+works%29%22
+
+// put your API key here;
+const apiKey = "qEIgONMDA9l4wPFA864Vvldpm2tfKzD2LJtAELFK";  
+
+// search base URL
+const searchBaseURL = "https://api.si.edu/openaccess/api/v1.0/search";
+
+// constructing the initial search query
+// const search =  'mask AND unit_code:"FSG"';
+// const search =  `Flowers AND unit_code:"CHNDM" AND object_type:"Embroidery (visual works)" AND online_media_type:"Images"`;
+const search =  `Woman AND unit_code:"NPG" AND online_media_type:"Images"`;
+
+// array that we will write into
+let myArray = [];
+
+// string that will hold the stringified JSON data
+let jsonString = '';
+
+// search: fetches an array of terms based on term category
+function fetchSearchData(searchTerm) {
+    let url = searchBaseURL + "?api_key=" + apiKey + "&q=" + searchTerm;
+    console.log(url);
+    window
+    .fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+     
+      // // constructing search queries to get all the rows of data
+      // // you can change the page size
+      // let pageSize = 1000;
+      // let numberOfQueries = Math.ceil(data.response.rowCount / pageSize);
+      // console.log(numberOfQueries)
+      // numberOfQueries = 200;
+      // for(let i = 0; i < numberOfQueries; i++) {
+      //   // making sure that our last query calls for the exact number of rows
+      //   if (i == (numberOfQueries - 1)) {
+      //     searchAllURL = url + `&start=${i * pageSize}&rows=${data.response.rowCount - (i * pageSize)}`;
+      //   } else {
+      //     searchAllURL = url + `&start=${i * pageSize}&rows=${pageSize}`;
+      //   }
+      //   console.log(searchAllURL)
+      //   fetchAllData(searchAllURL);
+      // }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+}
+
+// fetching all the data listed under our search and pushing them all into our custom array
+function fetchAllData(url) {
+  window
+  .fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+
+    data.response.rows.forEach(function(n) {
+      addObject(n);
+    });
+    jsonString += JSON.stringify(myArray);
+    console.log(myArray);
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+}
+
+// create your own array with just the data you need
+function addObject(objectData) {  
+  
+  // we've encountered that some places have data others don't
+  let currentPlace = "";
+  if(objectData.content.indexedStructured.place) {
+    currentPlace = objectData.content.indexedStructured.place[0];
+  }
+
+  myArray.push({
+    id: objectData.id,
+    title: objectData.title,
+    link: objectData.content.descriptiveNonRepeating.record_link,
+    place: currentPlace
+  })
+}
+
+
+fetchSearchData(search);
+
+
+//---------------------------UNIT CODES------------------------------
+// ACAH: Archives Center, National Museum of American History
+// ACM: Anacostia Community Museum
+// CFCHFOLKLIFE: Smithsonian Center for Folklife and Cultural Heritage
+// CHNDM: Cooper-Hewitt, National Design Museum
+// FBR: Smithsonian Field Book Project
+// FSA: Freer Gallery of Art and Arthur M. Sackler Gallery Archives
+// FSG: Freer Gallery of Art and Arthur M. Sackler Gallery
+// HAC: Smithsonian Gardens
+// HMSG: Hirshhorn Museum and Sculpture Garden
+// HSFA: Human Studies Film Archives
+// NAA: National Anthropological Archives
+// NASM: National Air and Space Museum
+// NMAAHC: National Museum of African American History and Culture
+// NMAfA: Smithsonian National Museum of African Art
+// NMAH: Smithsonian National Museum of American History
+// NMAI: National Museum of the American Indian
+// NMNHANTHRO: NMNH - Anthropology Dept.
+// NMNHBIRDS: NMNH - Vertebrate Zoology - Birds Division
+// NMNHBOTANY: NMNH - Botany Dept.
+// NMNHEDUCATION: NMNH - Education & Outreach
+// NMNHENTO: NMNH - Entomology Dept.
+// NMNHFISHES: NMNH - Vertebrate Zoology - Fishes Division
+// NMNHHERPS: NMNH - Vertebrate Zoology - Herpetology Division
+// NMNHINV: NMNH - Invertebrate Zoology Dept.
+// NMNHMAMMALS: NMNH - Vertebrate Zoology - Mammals Division
+// NMNHMINSCI: NMNH - Mineral Sciences Dept.
+// NMNHPALEO: NMNH - Paleobiology Dept.
+// NPG: National Portrait Gallery
+// NPM: National Postal Museum
+// SAAM: Smithsonian American Art Museum
+// SI: Smithsonian Institution, Digitization Program Office
+// SIA: Smithsonian Institution Archives
+// SIL: Smithsonian Libraries
---
+++ lab05-d3-json/04_Extra_d3_JamesTroxel/main.js
@@ -0,0 +1,49 @@
+let bins;
+let caratWeights =[]
+
+// Load the data
+d3.json('data/data-caratWeight.json').then(function(data){ 
+  data.forEach(function(d){
+    if(d.caratWeight!=null)
+    caratWeights.push(d.caratWeight);
+  })
+
+  // Define the bins
+  // we're having a cut-off at carathweight 100 and defining the number of bins at 17
+  let bin = d3.bin().domain([0, 100]).thresholds(17);
+
+  bins = bin(caratWeights);
+
+     // define dimensions and margins for the graphic
+     const margin = ({top: 100, right: 50, bottom: 100, left: 80})
+     const width = 1400;
+     const height = 700;
+
+  // Define the scales
+  x = d3.scaleLinear()
+    .domain([bins[0].x0, bins[bins.length - 1].x1])
+    .range([margin.left, width - margin.right])
+    
+  y = d3.scaleLinear()
+    .domain([0, d3.max(bins, d => d.length)])
+    .range([height - margin.bottom, margin.top])
+
+// DRAWING
+
+    // create an svg container from scratch
+    const svg = d3.select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+
+// attach a graphic element, and append rectangles to it
+svg.append("g")
+.attr("fill", "steelblue")
+.selectAll("rect")
+.data(bins)
+.join("rect")
+.attr("x", d => x(d.x0) + 1)
+.attr("width", d => Math.max(0, x(d.x1) - x(d.x0) - 1))
+.attr("y", d => y(d.length))
+.attr("height", d => y(0) - y(d.length));
+});
