9cdc8fcdf7ff31e9686f19ac9791d83a31c7eb62
Sohee Cho <soheecho94@gmail.com>
2020-10-07 11:50:14 -0400

    swap met data with smithsonian

--- lab06_images/p5_image_display/sketch.js
+++ lab06_images/p5_image_display/sketch.js
@@ -1,4 +1,4 @@
-let met_data;
+let smithsonian_data;
 let img_width = 80;
 let margin = 5;
 let images = [];
@@ -6,21 +6,21 @@ let folder = "images";
 let loadedImagesCount = 0;
 
 function setup() {
- 	createCanvas(800,600);
+ 	createCanvas(800, 600);
   // loading the json data. Since this is asynchronous, we use a callback for loading the images
-  met_data = loadJSON("data.json", loadImages);
+  smithsonian_data = loadJSON("data.json", loadImages);
 }
 
 // load all images
 function loadImages(){
-    for(var i=0; i<met_data.length; i++){
-      images[i] = loadImage(folder + "/" + met_data[i].filename,
+    for(var i=0; i<smithsonian_data.length; i++){
+      images[i] = loadImage(folder + "/" + smithsonian_data[i].filename,
       function(){
           // since these will load asynchronously, we have to keep track how many images have been loaded
           loadedImagesCount++;
 
           // once all have been loaded, draw the images
-          if(loadedImagesCount==met_data.length){
+          if(loadedImagesCount==smithsonian_data.length){
             drawImages();
           }
       }
@@ -33,8 +33,9 @@ function drawImages() {
   background(0);
   fill(255);
   textSize(8);
-  for(var i=0; i<met_data.length; i++){
+  for(var i=0; i<smithsonian_data.length; i++){
       image(images[i],i*20,i*20);
-      text(met_data[i].title, i*20,i*20);
+      text(smithsonian_data[i].title, i*20,i*20);
   }
 }
+
