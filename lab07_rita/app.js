7dfd7b9804c0efd2faeb30d4fae51e84657a9b17
Sohee Cho <soheecho94@gmail.com>
2020-10-13 14:41:03 -0400

    add rita lab

---
+++ lab07_rita/app.js
@@ -0,0 +1,45 @@
+/*
+Example by Dan Shiffman:
+https://www.youtube.com/watch?v=lIPEvh8HbGQ
+
+RiTa.js reference:
+https://rednoise.org/rita/reference/index.php
+*/
+
+function getInput() {
+  let input = document.getElementById("myInput").value;
+  processRita(input)
+}
+
+function processRita(input) {
+  // change our input to a Rita string
+  var rs = new RiString(input);
+
+  // break our phrase into words:
+  var words = rs.words();
+  console.log(words);
+
+  // get part-of-speech tags
+  // part-of-speech tags list: https://rednoise.org/rita/reference/PennTags.html
+  var pos = rs.pos();
+  console.log(pos);
+  
+  // change certain part-of-speech tags
+  let output = '';
+  words.forEach((word, i) => {
+    // use regular expression to replace all nouns with random words pulled from RiTa
+    if(/nn.*/.test(pos[i])) {
+      // if the word is a noun replace the word with a new noun:
+      output += RiTa.randomWord(pos[i]) + ' ';
+    } else {
+      // if not, return the original word:
+      output += word + ' ';
+    }
+  })
+
+  // adding p elements with d3
+  d3.select('#app')
+    .append('p')
+    .attr('class', 'rita-text')
+    .text(output);
+}
